590847262d1623b5bdd95ca09480c7c83067c816
zbrooks-marathonus <96266915+zbrooks-marathonus@users.noreply.github.com>
2021-12-16 16:13:52 -0700

    Update ItemController.cs

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -37,9 +37,9 @@ namespace WishList.Controllers
             return RedirectToAction("Index");
         }
 
-        public IActionResult Delete(int Id)
+        public IActionResult Delete(int id)
         {
-            var item = _context.Items.FirstOrDefault(c => c.Id == Id);
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
             _context.Items.Remove(item);
             _context.SaveChanges();
 
