d07ee284b71f3affad7740a09853742aa05cd031
zbrooks-marathonus <96266915+zbrooks-marathonus@users.noreply.github.com>
2021-12-16 15:25:07 -0700

    Added DataModels and DBModels

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public readonly DbSet<Item> Items;
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WishList.Data;
 
 namespace WishList
 {
@@ -18,6 +19,9 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseInMemoryDatabase("WishList")
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
