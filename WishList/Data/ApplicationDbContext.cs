2cd50a6bb90a625fb4ec209ebc961e263463faf6
zbrooks-marathonus <96266915+zbrooks-marathonus@users.noreply.github.com>
2021-12-16 15:26:57 -0700

    Update ApplicationDbContext.cs

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ namespace WishList.Data
 {
     public class ApplicationDbContext: DbContext
     {
-        public DbSet<Item> Items;
+        public DbSet<Item> Items { get; set; }
 
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
